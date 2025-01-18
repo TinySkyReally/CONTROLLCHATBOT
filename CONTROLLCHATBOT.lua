f62df19e722de73f48f24feb3ad749d41e276c02
TinySkyReally <tinyskyreally@gmail.com>
2025-01-18 02:15:11 +0100

    Update CONTROLLCHATBOT.lua

--- CONTROLLCHATBOT.lua
+++ CONTROLLCHATBOT.lua
@@ -67,9 +67,9 @@ local function sendMessage(text)
 end
 
 local function sendCommandList()
-    local commands = "Available Commands: hotkey [key] - Change the hotkey for commands, say [message] - Make the bot say something in chat, jump - Make the character jump, whitelist [player] - Add a player to the whitelist"
-    local secondcommands = "blacklist [player] - Remove a player from the whitelist, blacklist all - Clear the entire whitelist, reset - Reset the player's position, walkto [player] - Make the character walk to a player, cmds - Show the list of available commands"
-    local thirdcommands = "whitelist all - Add all players to the whitelist"
+    local commands = "Available Commands: hotkey [key] - Change the hotkey for commands, say [message] - Make the bot say something in chat, jump - Make the character jump"
+    local secondcommands = "whitelist [player] - Add a player to the whitelist, whitelist all - Add all players to the whitelist, blacklist [player] - Remove a player from the whitelist"
+    local thirdcommands = "blacklist all - Clear the entire whitelist, reset - Reset the player's position, walkto [player] - Make the character walk to a player, cmds - Show the list of available commands"
     
     sendMessage(commands)
     sendMessage(secondcommands)
