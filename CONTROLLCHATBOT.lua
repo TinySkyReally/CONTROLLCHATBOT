3819cdef1c565544d3de447e41078b0148c603b5
TinySkyReally <tinyskyreally@gmail.com>
2025-01-18 16:34:13 +0100

    Update CONTROLLCHATBOT.lua

--- CONTROLLCHATBOT.lua
+++ CONTROLLCHATBOT.lua
@@ -6,7 +6,7 @@ local Latest
 local TeleportService = game:GetService("TeleportService")
 local Name = "Tiny Control Bot"
 local Hotkey = "."
-local Whitelist = {}
+local Whitelist = {WakeMeUp, TinyControllBot}
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TextChatService = game:GetService("TextChatService")
